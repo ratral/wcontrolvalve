d3992126698845613377c6816a90f4148abc9322
Raul Trujillo <R.Trujillo@vag-group.com>
2020-07-08 11:36:19 +0200

    develop inv_LL3

--- R/valve_hydrailic_functions.R
+++ R/valve_hydrailic_functions.R
@@ -293,8 +293,32 @@
 #' @export
 #'
 #' @examples
-#' drm_LL3(5, 2.47033, 7.85543, 3.26336)
+#' drm_LL3( 50, -2.39, 1.39, 67.42 )
   drm_LL3 <- function(x,b,d,e){
     ll3 <- d/(1+exp(b*(log(x)-log(e))))
     return(ll3)
   }
+
+
+#' @title Inverse of the dose-response models.
+#' @description Built-in the inverse of dose-response models.
+#'
+#' @param kv_kvs valve position
+#' @param b steepness
+#' @param d upper value
+#' @param e the effective dose
+#'
+#' @importFrom stats uniroot
+#'
+#' @return position
+#' @export
+#'
+#' @examples
+#' inv_LL3( 0.45, -2.39, 1.39, 67.42 )
+  inv_LL3 <- function( kv_kvs, b, d, e ){
+      root <- uniroot( function(x){ d/(1+exp(b*(log(x)-log(e))))- kv_kvs} ,
+                       lower = 0,
+                       upper = 100,
+                       tol   = 0.0001)
+      return(root$root)
+    }
