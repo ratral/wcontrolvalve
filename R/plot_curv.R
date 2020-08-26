97660951412412c004200cf4f742028d69e33e71
Dr. Raul Trujillo Alvarez <dr.ing.trujillo@gmail.com>
2020-08-26 08:27:21 +0200

    Layout plot sigma

--- R/plot_curv.R
+++ R/plot_curv.R
@@ -137,15 +137,16 @@
   plot_sigma <- function( b, d, e, fls, cylindertyp){
     x   <- data.frame(x = 1:100)
     ggplot( data = x, mapping = aes(x = x)) +
-      stat_function( fun = function(x) {Sigma_mv(x, b, d, e, fls)}, size = 1, color = "red") +
-      stat_function( fun = function(x) {Sigma_c(x, b, d, e, fls)}, size = 1,  color = "orange") +
-      stat_function( fun = function(x) {Sigma_i(x, b, d, e, fls)}, size = 1,  color = "green") +
+      stat_function( fun = function(x) {Sigma_mv(x, b, d, e, fls)}, size = 1, aes(colour = "3.-Maximum")) +
+      stat_function( fun = function(x) {Sigma_c(x, b, d, e, fls)}, size = 1,  aes(colour = "2.-Constant")) +
+      stat_function( fun = function(x) {Sigma_i(x, b, d, e, fls)}, size = 1,  aes(colour = "1.-Incipient")) +
+      scale_colour_manual("Borders of the cavitation: ", values = c("green", "orange", "red")) +
       scale_x_continuous( breaks = seq(0, 100, 10)) +
-      labs( title    = TeX('Borders of the Sigma value ($\\sigma$'),
+      labs( title    = TeX('Sigma values ($\\sigma$)'),
             subtitle = paste("Flow characteristics :", cylindertyp),
             caption  = "Dr.Trujillo",
             x        = "Opening degree (%)",
-            y        = TeX('$F_{L}$')) +
-      theme_bw()
+            y        = TeX('Sigma values ($\\sigma$)')) +
+      theme_bw() + theme(legend.position = "bottom")
   }
 
