735e36ab9fe9f7b8c249dba19c00cde7b3ae4436
Dr. Raul Trujillo Alvarez <dr.ing.trujillo@gmail.com>
2020-08-28 14:26:23 +0200

    create data for Reynolds factor

---
+++ R/reynolds_factor.R
@@ -0,0 +1,19 @@
+
+#' @title Reynolds number factor for valve sizing.
+#'
+#' @description A \code{tibble} containing the values for the calculation and plot
+#' of the Valve Reynolds Number Factor. The table  show the relationships between
+#' FR and the valve Reynolds number Rev for the three types of problems that
+#' may be encountered with viscous flow. These are:
+#' a) Determining the required flow coefficient when selecting a control valve size
+#' b) Predicting the flow rate that a selected valve will pass
+#' c) Predicting the pressure differential that a selected valve will exhibit
+#'
+#' @format A \code{tibble} with 265 rows and 4 variables:
+#' \describe{
+#'   \item{problem_typ}{types of problems: "selection", "flow", "pressure"}
+#'   \item{reynolds}{Valve Reynolds Number}
+#'   \item{fr}{Reynolds number factor }
+#' }
+#' @source ISA–75.01–1985 (R1995) "Flow Equations for Sizing Control Valves"
+"reynolds_factor"
---
+++ data-raw/reynolds_factor.R
@@ -0,0 +1,17 @@
+
+  library(tidyverse)
+  library(here)
+
+  reynolds_factor <- read_csv(here::here("data-raw", "fr.csv"),
+    col_types = cols(problem_typ =
+                      col_factor(levels = c("selection", "flow", "pressure"))))
+
+  usethis::use_data(reynolds_factor,
+                    compress = "xz",
+                    overwrite = TRUE)
+
+  # https://www.r-bloggers.com/interpolation-and-smoothing-functions-in-base-r/
+
+  # reynolds_factor %>%
+  #   filter(problem_typ == "selection") %>%
+  #   with(spline(reynolds, fr, xout = c(340)))$y
